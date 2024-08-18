958af16da8ffa56117285c0ca1b54bd5d391b6d9
ExpertNugget <deaconjf04@gmail.com>
2024-08-17 22:34:39 -0600

    Voice to text

--- src/main.py
+++ src/main.py
@@ -1,2 +1,16 @@
 # GOAL: Make an AI voice assistant with multi input/output relative to the input location.
 # Future goals would include task specific requests acting as different "personalities" to change speed, voice, and style of reply according to the task, as well as visual inputs/outputs for varied tasks.
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
