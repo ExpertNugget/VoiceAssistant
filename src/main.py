f39f2055cbbb30599abdbf917e230222c0c6bc19
ExpertNugget <deaconjf04@gmail.com>
2024-09-17 10:25:51 -0600

    moved goals to README.md

--- src/main.py
+++ src/main.py
@@ -1,5 +1,3 @@
-# GOAL: Make an AI voice assistant with multi input/output relative to the input location.
-# Future goals would include task specific requests acting as different "personalities" to change speed, voice, and style of reply according to the task, as well as visual inputs/outputs for varied tasks.
 import speech_recognition as sr
 
 
